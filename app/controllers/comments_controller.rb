96abbc513681dbb5ab8fb1b4b4def7eb454b17e5
m0ribe <moro.recr@gmail.com>
2020-09-11 00:40:23 +0900

    add strong parameter

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,13 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+    @comment = Comment.create(comment_params)
     binding.pry
+    redirect_to root_path
   end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, request_id: params[:request_id])
+  end
+
 end
