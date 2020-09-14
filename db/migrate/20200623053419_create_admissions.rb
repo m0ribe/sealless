2c923133f41fa375798763bde381483daafb8569
m0ribe <moro.recr@gmail.com>
2020-09-14 22:00:56 +0900

    remove detail and title from deit, new and column

--- db/migrate/20200623053419_create_admissions.rb
+++ db/migrate/20200623053419_create_admissions.rb
@@ -2,10 +2,8 @@ class CreateAdmissions < ActiveRecord::Migration[6.0]
   def change
     create_table :admissions do |t|
       t.references :request, foreign_key: true
-      t.string :title, null: false
       t.datetime :start, null: false
       t.datetime :finish, null: false
-      t.string :detail, null: false
       t.string :prace, null: false
       t.string :campany, null: false
       t.string :representative, null: false
